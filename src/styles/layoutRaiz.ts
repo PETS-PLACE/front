a7bf6a8cd161810a9c65618bf0c0df06bbb2be79
ThalesAdriel <115805292+ThalesAdriel@users.noreply.github.com>
2023-12-01 00:04:42 +0000

    Feat: Construção do painel de usuário
    Criação do painel para o usuário
    Algums pequenos ajustes no layoutRaiz.ts
    
    obs: Tive um problme com o git e tive que fazer o upload manual

--- src/styles/layoutRaiz.ts
+++ src/styles/layoutRaiz.ts
@@ -4,7 +4,11 @@ import { paletaWeb } from './global';
 export const { styles } = StyleSheet.create({
   header:{
     backgroundColor: paletaWeb.roxoAlpha,
-    padding: 10
+    padding: 10,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    boxShadow: "0px 7px 15px 0px rgba(0, 0, 0, 0.25)",
+    flexDirection: "row",
   },
   footer:{
     backgroundColor: paletaWeb.roxoBeta,
