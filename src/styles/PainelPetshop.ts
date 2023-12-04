78a472c228f4574e7aff083382e9bcda0f75c3e0
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2023-12-04 10:49:32 -0300

    hotfix(painel): corrige equivoco da ramificacao feat-tela-painelusuario
    Reordena estrutura de diretorios do projeto.
    Adiciona painel do petshop e sua estilizacao para
    que outros integrantes possam trabalhar sem complicacao.
    Testa funcionamento ate o momento.

---
+++ src/styles/PainelPetshop.ts
@@ -0,0 +1,10 @@
+import StyleSheet from "react-native-media-query";
+
+//estilização do painel do usuário
+export const { styles } = StyleSheet.create({
+    demonstracao:{
+      color: '#ff0000'
+    }
+  }
+);
+
